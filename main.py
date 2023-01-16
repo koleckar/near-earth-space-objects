dcfe368584ead997345dc83d090fd5ddf6df966b
David Kolečkář <koleckar@protonmail.com>
2023-01-16 16:32:02 +0100

    REST API GET service working. Dates validation tested, longer period than 7 days handled by returning results from <start_day, start_day + 7 days>. Minor refactoring.

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import requests
 from flask import Flask
 from flask_restful import Api, Resource
 
+logging.basicConfig(level=logging.INFO)
+
 app = Flask(__name__)
 api = Api(app)
 
@@ -13,7 +15,7 @@ NASA_API_KEY = "v9IuYe5UNjJUikdhNfapmn8HhRYJvATWCCSQIYhQ"
 NASA_NEOWS_BASE_URL = "https://api.nasa.gov/neo/rest/v1/feed"
 
 
-def is_date_valid(date: str):
+def is_date_valid(date: str) -> bool:
     '''
     :param start_date: date in format YYYY-MM-DD
     :param end_date: date in format YYYY-MM-DD
@@ -26,18 +28,35 @@ def is_date_valid(date: str):
     return True
 
 
-class SpaceObjectController(Resource):
+def clip_end_date_to_seven_days(start_date: str, end_date: str) -> str:
+    datetime_start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d')
+    datetime_end_date = datetime.datetime.strptime(end_date, '%Y-%m-%d')
+
+    day_span = (datetime_end_date - datetime_start_date).days
+
+    return end_date if day_span <= 7 else str((datetime_start_date + datetime.timedelta(days=7)).date())
+
+
+class RestController(Resource):
     def get(self, start_date: str, end_date: str):
-        # if not is_date_valid(end_date) return invalid response
-        # if not is_date_valid(start_date) return invalid response
+        logging.info(f"/space_objects GET start_date={start_date}, end_date={end_date}")
 
-        return get_sorted_response(get_NeoWs_rest_response(start_date, end_date))
+        if not is_date_valid(start_date):
+            return [{"error": "invalid start_date"}], 400
+        if not is_date_valid(end_date):
+            return [{"error": "invalid end_date"}], 400
+
+        end_date = clip_end_date_to_seven_days(start_date, end_date)
+        print(start_date)
+        print(end_date)
+
+        return sort_response(get_nasa_neows_rest_api_response(start_date, end_date))
 
 
-api.add_resource(SpaceObjectController, "/space_objects")
+api.add_resource(RestController, "/space_objects/<string:start_date>/<string:end_date>")
 
 
-def get_NeoWs_rest_response(start_date, end_date):
+def get_nasa_neows_rest_api_response(start_date: str, end_date: str):
     request_url = NASA_NEOWS_BASE_URL + "?" \
                   + "start_date=" + start_date + "&end_date=" + end_date + "&api_key=" + NASA_API_KEY
 
@@ -45,7 +64,18 @@ def get_NeoWs_rest_response(start_date, end_date):
     return response
 
 
-def get_sorted_response(nasa_api_response):
+def sort_response(nasa_api_response) -> list:
+    '''
+    Space objects, sorted by their closest approach distance to Earth,
+    each containing object name, size estimate, time and distance of the closest encounter.
+
+    sorting algorithm:
+    1. flatten the first dictionary having just big list of all entries.
+    2. get all indexes sorted by entry["close_approach_data"][0]["miss_distance"]["kilometers"]
+
+    :param nasa_api_response: Response from GET request to "https://api.nasa.gov/neo/rest/v1/feed"
+    :return: list of dicts, keys = {name', 'size_estimate', closest_encounter_time', closest_encounter_distance'}
+    '''
     near_earth_objects = nasa_api_response.json()['near_earth_objects']
 
     entries_all_days = []
@@ -75,22 +105,16 @@ def get_sorted_response(nasa_api_response):
 if __name__ == '__main__':
     app.run(debug=True)
 
-# todo:
-#   1. flatten the first dictionary having just big list of all entries.
-#   2. get all indexes will
-#   3. !handle missing values during sorting and outputting object values!
-#   4. implement cache?
+# Todo:
+#   !handle missing values during sorting and outputting object values!
+#   implement cache?
 
 """
- Output the list of the objects, sorted by their closest approach distance, 
-    containing: 
-        object name
-        size estimate
-        time and distance of the closest encounter
+Nasa NeoWs REST api response structure:
 
-# near_earth_objects (dict): keys=consecutive days
-# consecutive day(list) of space objects
-# space object (dict): dict_keys([
+near_earth_objects (dict): keys=consecutive days
+consecutive day(list) of space objects
+space object (dict): dict_keys([
     'links', 
     'id', 
     'neo_reference_id', 
@@ -120,6 +144,7 @@ We sort by 'miss_distance' (dict) : dict_keys([
     'kilometers',
     'miles'
     ]) 
+    
 we will use kilometers
 
 entry_dist_km = near_earth_objects["2023-01-01"][entry]["close_approach_data"][0]["miss_distance"]["kilometers"])
--- test.py
+++ test.py
@@ -1,7 +1,13 @@
 import requests
 
 BASE = "http://127.0.0.1:5000"
+start_date = "2023-01-01"
+end_date = "2023-01-30"
 
-response = requests.get(BASE + "/space_objects")
-print(response.json())
+#response = requests.get(BASE + "/space_objects?" + "start_date=" + start_date + "&end_date=" + end_date)
+response = requests.get(BASE + "/space_objects" + "/" + start_date + "/" + end_date)
+print(response.status_code)
+for entry in response.json():
+    for key, value in entry.items():
+        print(f"{key}: {value}")
 
