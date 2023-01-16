6469a8f2da3cc98f5a664a8109139d0ef34b1c85
David Kolečkář <koleckar@protonmail.com>
2023-01-16 18:44:03 +0100

    Changed day span clipping. Multiple GET calls to nasa api, for each consecutive 7 days in given date range(last pair can be shorter). According refactoring.

--- main.py
+++ main.py
@@ -14,11 +14,12 @@ api = Api(app)
 NASA_API_KEY = "v9IuYe5UNjJUikdhNfapmn8HhRYJvATWCCSQIYhQ"
 NASA_NEOWS_BASE_URL = "https://api.nasa.gov/neo/rest/v1/feed"
 
+MAX_DAY_SPAN = 365
+
 
 def is_date_valid(date: str) -> bool:
     '''
-    :param start_date: date in format YYYY-MM-DD
-    :param end_date: date in format YYYY-MM-DD
+    :param date: (str) date in format YYYY-MM-DD
     :return: true if date in format YYYY-MM-DD, false otherwise
     '''
     try:
@@ -28,6 +29,7 @@ def is_date_valid(date: str) -> bool:
     return True
 
 
+@DeprecationWarning
 def clip_end_date_to_seven_days(start_date: str, end_date: str) -> str:
     datetime_start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d')
     datetime_end_date = datetime.datetime.strptime(end_date, '%Y-%m-%d')
@@ -37,8 +39,17 @@ def clip_end_date_to_seven_days(start_date: str, end_date: str) -> str:
     return end_date if day_span <= 7 else str((datetime_start_date + datetime.timedelta(days=7)).date())
 
 
+def get_day_span(start_date: str, end_date: str) -> int:
+    datetime_start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d')
+    datetime_end_date = datetime.datetime.strptime(end_date, '%Y-%m-%d')
+    day_span = (datetime_end_date - datetime_start_date).days
+    assert day_span >= 0, "Negative day span!"
+    return day_span
+
+
 class RestController(Resource):
     def get(self, start_date: str, end_date: str):
+
         logging.info(f"/space_objects GET start_date={start_date}, end_date={end_date}")
 
         if not is_date_valid(start_date):
@@ -46,17 +57,64 @@ class RestController(Resource):
         if not is_date_valid(end_date):
             return [{"error": "invalid end_date"}], 400
 
-        end_date = clip_end_date_to_seven_days(start_date, end_date)
-        print(start_date)
-        print(end_date)
+        nasa_responses = get_nasa_responses(start_date, end_date)
 
-        return sort_response(get_nasa_neows_rest_api_response(start_date, end_date))
+        return sort_response(nasa_responses)
 
 
 api.add_resource(RestController, "/space_objects/<string:start_date>/<string:end_date>")
 
 
-def get_nasa_neows_rest_api_response(start_date: str, end_date: str):
+def get_date_pairs(start_date: str, end_date: str) -> list:
+    day_span = get_day_span(start_date, end_date)
+    day_span = min(day_span, MAX_DAY_SPAN)  # todo: signal span clipping in GET response?
+
+    date_pair = []
+    if day_span <= 7:
+        date_pair.append([start_date, end_date])
+        return date_pair
+
+    datetime_start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d')
+    date_pairs = []
+
+    i_start = 0
+    in_span = True
+    while in_span:
+        if i_start + 7 >= day_span:
+            in_span = False
+
+        span = min(6, max(0, day_span - i_start))
+        date_pairs.append([
+            str((datetime_start_date + datetime.timedelta(days=i_start)).date()),
+            str((datetime_start_date + datetime.timedelta(days=i_start + span)).date())
+        ])
+        i_start += 7
+
+    logging.info(date_pairs)
+    return date_pairs
+
+
+def get_nasa_responses(start_date: str, end_date: str) -> list:
+    '''
+    Calls nasa rest service (multiple times if needed). Limit is set to MAX_DAY_SPAN=365
+
+    :return: list of entries from all days, aggregated from all nasa responses.
+    '''
+    nasa_responses = []
+    for start, end in get_date_pairs(start_date, end_date):
+        logging.info(f"making nasa GET call")
+        nasa_responses.append(get_nasa_response(start, end))
+
+    entries_all_days = []
+    for nasa_response in nasa_responses:
+        for entries_of_one_day in nasa_response.json()['near_earth_objects'].values():
+            for entry in entries_of_one_day:
+                entries_all_days.append(entry)
+
+    return entries_all_days
+
+
+def get_nasa_response(start_date: str, end_date: str):
     request_url = NASA_NEOWS_BASE_URL + "?" \
                   + "start_date=" + start_date + "&end_date=" + end_date + "&api_key=" + NASA_API_KEY
 
@@ -64,7 +122,7 @@ def get_nasa_neows_rest_api_response(start_date: str, end_date: str):
     return response
 
 
-def sort_response(nasa_api_response) -> list:
+def sort_response(entries_all_days: list) -> list:
     '''
     Space objects, sorted by their closest approach distance to Earth,
     each containing object name, size estimate, time and distance of the closest encounter.
@@ -73,16 +131,9 @@ def sort_response(nasa_api_response) -> list:
     1. flatten the first dictionary having just big list of all entries.
     2. get all indexes sorted by entry["close_approach_data"][0]["miss_distance"]["kilometers"]
 
-    :param nasa_api_response: Response from GET request to "https://api.nasa.gov/neo/rest/v1/feed"
-    :return: list of dicts, keys = {name', 'size_estimate', closest_encounter_time', closest_encounter_distance'}
+    :param entries_all_days: Response from possible multiple GET request to "https://api.nasa.gov/neo/rest/v1/feed"
+    :return: list of dicts, keys = {'name', 'size_estimate', 'closest_encounter_time', 'closest_encounter_distance'}
     '''
-    near_earth_objects = nasa_api_response.json()['near_earth_objects']
-
-    entries_all_days = []
-    for entries_of_one_day in near_earth_objects.values():
-        for entry in entries_of_one_day:
-            entries_all_days.append(entry)
-
     miss_distances_in_km = np.zeros(len(entries_all_days))
     for i in range(len(entries_all_days)):
         miss_distances_in_km[i] = entries_all_days[i]["close_approach_data"][0]["miss_distance"]["kilometers"]
@@ -108,44 +159,4 @@ if __name__ == '__main__':
 # Todo:
 #   !handle missing values during sorting and outputting object values!
 #   implement cache?
-
-"""
-Nasa NeoWs REST api response structure:
-
-near_earth_objects (dict): keys=consecutive days
-consecutive day(list) of space objects
-space object (dict): dict_keys([
-    'links', 
-    'id', 
-    'neo_reference_id', 
-    'name', 
-    'nasa_jpl_url', 
-    'absolute_magnitude_h', 
-    'estimated_diameter', 
-    'is_potentially_hazardous_asteroid', 
-    'close_approach_data', 
-    'is_sentry_object'
-    ])
-    
-close_approach_data is our value for sorting entries
-
-close_approach_data list of 1 element = (dict) : dict_keys([
-    'close_approach_date', 
-    'close_approach_date_full', 
-    'epoch_date_close_approach',
-    'relative_velocity', 
-    'miss_distance', 
-    'orbiting_body'
-    ])
-     
-We sort by 'miss_distance' (dict) : dict_keys([
-    'astronomical',
-    'lunar',
-    'kilometers',
-    'miles'
-    ]) 
-    
-we will use kilometers
-
-entry_dist_km = near_earth_objects["2023-01-01"][entry]["close_approach_data"][0]["miss_distance"]["kilometers"])
-"""
+#   partial sort while waiting for nasa GET response?
